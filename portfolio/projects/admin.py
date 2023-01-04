142e095855a4fc202e8808bfbad57f5f3bf75caa
jogorska <gorska919@gmail.com>
2023-01-04 23:58:45 +0000

    Add admin for models in projects

--- portfolio/projects/admin.py
+++ portfolio/projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import Project, Language
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+
+
+class LanguageAdmin(admin.LanguageAdmin):
+    model = Language
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.stie.register(Language, LanguageAdmin)
