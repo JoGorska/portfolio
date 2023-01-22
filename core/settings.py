ac41fcd2cb4e971144b3c51a5b018923a087e01e
jogorska <gorska919@gmail.com>
2023-01-22 22:36:23 +0000

    Add django crispy forms

--- core/settings.py
+++ core/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
+    "crispy_forms",
+    "crispy_bootstrap5",
     # allauth
     'django.contrib.sites',
     'allauth',
@@ -59,6 +61,10 @@ INSTALLED_APPS = [
     'projects',
 
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
